5e13472d13207601b50f963478cf2417431b065c
Meezaan-ud-Din <meezaan@vesica.co.uk>
2017-10-20 15:27:23 +0100

    Dev mode enabled by default

--- config/doctrineBootstrap.docker.php
+++ config/doctrineBootstrap.docker.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
 $paths = array(realpath(__DIR__) . '/../src');
-$isDevMode = false;
+$isDevMode = true;
 
 // the connection configuration
 
