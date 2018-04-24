9be0749171d14908484da4a6771a0c4934c0d993
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2018-04-24 07:32:28 +0100

    https check updated

--- src/Quran/Helper/Request.php
+++ src/Quran/Helper/Request.php
@@ -27,18 +27,22 @@ class Request
 
     public static function isHttps()
     {
-        if ( isset($_SERVER) && isset($_SERVER['HTTPS']) ) {
-            if ( 'on' == strtolower($_SERVER['HTTPS']) ) {
+        if ( isset($_SERVER) ) {
+            if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
                 return true;
             }
-            if ( '1' == $_SERVER['HTTPS'] ) {
+            if ( isset($_SERVER['HTTPS']) ) {
+                if ( 'on' == strtolower($_SERVER['HTTPS']) ) {
+                    return true;
+                }
+                if ( '1' == $_SERVER['HTTPS'] ) {
+                    return true;
+                }
+            } elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
                 return true;
             }
-        } elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
-            return true;
-        }
 
-        return false;
-    }
+            return false;
+        }
 
 }
