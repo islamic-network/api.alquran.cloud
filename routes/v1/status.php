c35b8438918196821e43832cbdd1c2e7aaf81624
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2018-09-02 18:36:39 +0100

    fixed request error

---
+++ routes/v1/status.php
@@ -0,0 +1,19 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Quran\Helper\Log;
+use Quran\Helper\Request as ApiRequest;
+
+$app->group('/v1', function() {
+    // With Ayat Number or All
+    $this->get('/status', function (Request $request, Response $response) {
+
+        $number = rand(1, 6326);
+        $edition = 'quran-simple';
+        $ayat = new Quran\Api\AyatResponse($number, $edition);
+        $this->logger->addInfo('ayah ::: ' . time() . ' ::', Log::format($_SERVER, $_REQUEST));
+
+        return $response->withJson($ayat->get(), $ayat->getCode());
+    });
+
+});
