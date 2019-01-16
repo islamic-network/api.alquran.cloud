3af2711329b103855fb0e270a42b336590952ca7
Meezaan-ud-Din <meezaan@giffgaff.co.uk>
2019-01-16 15:00:42 +0000

    bumping build for new base image

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Release tag / version. If this is not for a specific release, please set this to latest, otherwise set it to a specific release.
-version=8
+version=9
 
 ###########################################################################################
 #UNLESS YOU WANT TO CHANGE SOMETHING TO DO WITH THE PUSH TO THE REGISTRY, LEAVE THE BELOW ALONE #
