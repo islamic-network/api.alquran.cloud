917a86e0e71fa23f47a311c2c9f1ebb0a58afbab
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-09-07 13:16:05 +0400

    Turn off debugger

--- config/kipchak.api.php
+++ config/kipchak.api.php
@@ -6,5 +6,5 @@ return [
     'name' => 'alquran-api', // Hyphen or underscore separated string
     'debug' => (bool) env('DEBUG', false),
     // If debug is enabled, loglevel is debug. Otherwise, it is info. Overwrite it by specifying it below.
-    'loglevel' => \Monolog\Level::Debug
+    // 'loglevel' => \Monolog\Level::Debug
 ];
