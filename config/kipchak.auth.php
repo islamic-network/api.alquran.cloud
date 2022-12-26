6694b314370707b60fdd38a206e82854c87f187b
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-12-26 16:45:40 +0400

    Update kipchak version

--- config/kipchak.auth.php
+++ config/kipchak.auth.php
@@ -1,10 +1,11 @@
 <?php
-
-use function Mamluk\Kipchak\env;
-
 return [
     'jwks' => [
         'enabled' => false, // to enable this globally
+        'ignore_options' => true, // Setting this to true will enable preflight requests with the OPTIONS HTTP method without authentication
+        'ignore_paths' => [
+            '/status'
+        ],
         'jwksUri' => 'https://auth.islamic.network/auth/realms/islamic-network/protocol/openid-connect/certs',
         'validate_scopes' => true, // If enabled, the following scopes will be validated unless custom ones are passed to the AuthWJKS Middleware
         'scopes' => [
@@ -14,11 +15,13 @@ return [
     ],
     'key' => [
         'enabled' => false, // Will check for key in key query parameter (?key=xxxxxxx) or x-api-key header globally
+        'ignore_options' => true, // Setting this to true will enable preflight requests with the OPTIONS HTTP method without authentication
+        'ignore_paths' => [
+            '/status'
+        ],
         'authorised_keys' => [
-            // This format is chosen because it's faster than array with just keys. The values key1, key2 or key3 will be checked
-            'key1' => 'client1',
-            'key2' => 'client2',
-            'key3' => 'client3'
+            // This format is chosen because it's faster than an array with just keys. The values key1, key2 or key3 will be checked
+            // 'key2' => 'client2',
         ]
     ]
 ];
