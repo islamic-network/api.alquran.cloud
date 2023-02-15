91ac62cd82c08bceb6dceb2afa6a59f3b2edcb6e
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2023-02-15 11:26:40 +0400

    Update Kipchak

--- config/kipchak.memcached.php
+++ config/kipchak.memcached.php
@@ -10,12 +10,17 @@ return [
                 'host' => env('MEMCACHED_HOST', 'memcached'),
                 'port' => env('MEMCACHED_PORT', 11211),
             ]
-        ],
-        'sessions' => [
-            [
-                'host' => env('MEMCACHED_HOST', 'memcached'),
-                'port' => env('MEMCACHED_PORT', 11211),
-            ]
+        ]
+    ],
+    'pools_options' => [
+        'cache' => [
+            Memcached::OPT_PREFIX_KEY => '',
+            Memcached::OPT_CONNECT_TIMEOUT => 1000, // millisconeds
+            Memcached::OPT_RETRY_TIMEOUT => 1, // seconds
+            Memcached::OPT_POLL_TIMEOUT => 1000, // milliseconds
+            Memcached::OPT_SERVER_FAILURE_LIMIT => 3,
+            Memcached::OPT_REMOVE_FAILED_SERVERS => true,
+            Memcached::OPT_DISTRIBUTION => Memcached::DISTRIBUTION_CONSISTENT
         ]
     ]
 ];
