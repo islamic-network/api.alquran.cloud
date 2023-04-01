19690bb769a8f94bdc3f6ee9268a53187ad53b33
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2023-04-01 11:22:59 +0400

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,5 +1,10 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, origin, if-none-match');
+header('Access-Control-Expose-Headers: *');
 
 require_once (realpath(__DIR__ . '/../vendor/autoload.php'));
 
